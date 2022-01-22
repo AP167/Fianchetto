03056eb808912172a4c059dc46ebc47f663a1e3c
Debopriya Roy <dpriya.jsr@gmail.com>
2022-01-22 21:09:13 +0530

    signin

---
+++ src/components/sign_in/signIn.style.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Box } from "@mui/system";
+
+export const fullScreenBox = styled(Box)`
+
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .centreText {
+//     text-align: center;
+// }
+// .leftTeft {
+//     text-align: left;
+//     font-family: 'Grenze Gotisch', cursive;
+// }
+// .rightText {
+//     text-align: right;
+// }
+// .brownGrad {
+//     min-height: 100vh;
+//     background: linear-gradient(116.53deg, #F4991A 0%, rgba(50, 19, 19, 0.9) 99.58%);
+// }
+// .lightBox {
+//     color: #F2EAD3;
+// }
+/* body {
+    min-height: 100vh;
+    background: linear-gradient(116.53deg, #F4991A 0%, rgba(50, 19, 19, 0.9) 99.58%);
+} */
